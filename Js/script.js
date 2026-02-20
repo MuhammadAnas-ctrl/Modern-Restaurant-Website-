0839ec3b73de33dad977f8441668f8d47d96529d
Anas <flashycoderch@gmail.com>
2026-02-20 07:39:58 +0500

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,8 @@
+function toggleMenu() {
+  document.getElementById("nav").classList.toggle("active");
+}
 
+window.addEventListener("scroll", function() {
+  const header = document.querySelector("header");
+  header.style.background = window.scrollY > 50 ? "#000" : "transparent";
+});
